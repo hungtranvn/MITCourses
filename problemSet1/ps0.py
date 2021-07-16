7a325b42373334fadef6c2db518c71cce069267b
Hung Tran <hungtran.vnu@gmail.com>
2021-07-16 08:40:30 +0700

    add some .py files for problem 0

---
+++ problemSet1/ps0.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+input and output problem
+Created on Sat Apr 13 23:11:19 2019
+@author: hungtran
+"""
+import math
+
+x = int(input("Enter number x: "))
+y = int(input("Enter number y: "))
+print("x**y = ", x**y)
+print("log(x) = ", math.log(x,2))
